a09ee9f817c0716e24d4732b8b390907f654a1a2
gu18168 <gu18168@gmail.com>
2017-01-26 14:43:49 +0800

    添加圆角按钮与背景图片

--- app/src/main/java/com/gyh/login/MainActivity.java
+++ app/src/main/java/com/gyh/login/MainActivity.java
@@ -1,15 +1,45 @@
 package com.gyh.login;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView bgPicImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_main);
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        bgPicImg = (ImageView) findViewById(R.id.bg_img);
+        String bgPic = prefs.getString("bg_pic", null);
+        if (bgPic != null) {
+            Glide.with(this).load(bgPic).into(bgPicImg);
+        } else {
+            loadBgPic();
+        }
+    }
 
+    /**
+     * 为以后网络接收图片做接口
+     */
+    private void loadBgPic() {
+        Glide.with(this).load(R.drawable.bg).into(bgPicImg);
     }
 }
