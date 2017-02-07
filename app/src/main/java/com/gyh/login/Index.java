6b2613526a46f6fab3703613fc208da27f06b077
gu18168 <gu18168@gmail.com>
2017-02-07 19:29:44 +0800

    增加侧边栏

--- app/src/main/java/com/gyh/login/Index.java
+++ app/src/main/java/com/gyh/login/Index.java
@@ -1,11 +1,16 @@
 package com.gyh.login;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
 import com.gyh.login.db.Ad;
 import com.gyh.login.util.AdLab;
 import com.gyh.login.util.OnPageClickListener;
@@ -17,8 +22,11 @@ import java.util.List;
 public class Index extends AppCompatActivity {
 
     private List<ImageView> mViews = new ArrayList<>();
+
     private ViewPagerAdapter mAdapter;
     private BannerViewPager mBannerViewPager;
+    private DrawerLayout mDrawerLayout;
+    private FloatingSearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,32 @@ public class Index extends AppCompatActivity {
             }
         });
         mBannerViewPager.setAdapter(mAdapter);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mSearchView= (FloatingSearchView) findViewById(R.id.search_view);
+
+        // 搜索逻辑
+        mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+
+                //get suggestions based on newQuery
+
+                //pass them on to the search view
+//                mSearchView.swapSuggestions(newSuggestions);
+            }
+        });
+        // 添加侧边栏
+        mSearchView.attachNavigationDrawerToMenuButton(mDrawerLayout);
+
+        // 侧边栏点击逻辑
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 
     private void createViews() {
